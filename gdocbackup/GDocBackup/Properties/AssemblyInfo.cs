21ccd713d8f0db99d6099f0e466095e17475fd44
fhtino <fhtino@6a28fcac-3018-11de-a0ce-9586e7784243>
2009-07-30 09:20:05 +0000

    work in progress...

--- gdocbackup/GDocBackup/Properties/AssemblyInfo.cs
+++ gdocbackup/GDocBackup/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GDocBackup")]
-[assembly: AssemblyDescription("http://gs.fhtino.it/gdocbackup")]
+//[assembly: AssemblyDescription("http://gs.fhtino.it/gdocbackup")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GDocBackup")]
-[assembly: AssemblyCopyright("Copyright ©  2009  Fabrizio Accatino")]
+//[assembly: AssemblyCopyright("Copyright ©  2009  Fabrizio Accatino")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
