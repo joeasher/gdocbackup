2943c16ae85ac03aeb58360b90d59a63dfa8e004
fhtino <fhtino@6a28fcac-3018-11de-a0ce-9586e7784243>
2009-10-06 14:47:38 +0000

    ...working

--- trunk/gdocbackup/GDocBackup/AboutForm.cs
+++ trunk/gdocbackup/GDocBackup/AboutForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Reflection;
+using System.Threading;
 
 namespace GDocBackup
 {
@@ -20,16 +21,51 @@ namespace GDocBackup
         {
             this.Icon = Properties.Resources.Logo;
             LblVersion.Text += Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            Thread checkNewVersionThrd = new Thread(ExecCheckNewVersion);
+            checkNewVersionThrd.Name = "CheckNewVersionExists";
+            checkNewVersionThrd.IsBackground = true;
+            checkNewVersionThrd.Start();
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start((sender as LinkLabel).Text);
-        }        
+        }
 
         private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start((sender as LinkLabel).Text);
         }
+
+        private void ExecCheckNewVersion()
+        {
+            Version localVersion;
+            _newveravailable = CheckUpdates.Exec(out localVersion, out _newversion);
+            try
+            {
+                Thread.Sleep(5);
+                this.BeginInvoke((MethodInvoker)delegate() { this.ShowCheckNewVersionResult(); });
+            }
+            catch (Exception) { }   // the form could be closed during thread run
+        }
+
+        private bool _newveravailable;
+        private Version _newversion;
+
+        private void ShowCheckNewVersionResult()
+        {
+            if (_newveravailable)
+            {
+                lblCheckNewVer.Text = "A new version is available (" + _newversion.ToString() + ")";
+                pictRotor.Image = Properties.Resources.warning_20x20;
+            }
+            else
+            {
+                lblCheckNewVer.Text = "You have the latest version.";
+                pictRotor.Image = Properties.Resources.ok_20x20;
+            }
+        }
+
     }
 }
