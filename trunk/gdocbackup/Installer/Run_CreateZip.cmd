966e10799e350160115560257fa539f46bbdf802
fhtino <fhtino@6a28fcac-3018-11de-a0ce-9586e7784243>
2010-02-08 13:14:47 +0000

    (maintenance)

--- trunk/gdocbackup/Installer/Run_CreateZip.cmd
+++ trunk/gdocbackup/Installer/Run_CreateZip.cmd
@@ -3,6 +3,6 @@
 DEL  GDocBackup_$$$VerMajor$$$.$$$VerMinor$$$.$$$VerBuild$$$.$$$VerRevision$$$_BIN.zip
 DEL  GDocBackup_$$$VerMajor$$$.$$$VerMinor$$$.$$$VerBuild$$$.$$$VerRevision$$$_CMD.zip 
 
-"c:\Program Files\7-Zip\7z.exe"    a   GDocBackup_$$$VerMajor$$$.$$$VerMinor$$$.$$$VerBuild$$$.$$$VerRevision$$$_BIN.zip     .\CompiledBIN\*.exe   .\CompiledBIN\*.dll
-"c:\Program Files\7-Zip\7z.exe"    a   GDocBackup_$$$VerMajor$$$.$$$VerMinor$$$.$$$VerBuild$$$.$$$VerRevision$$$_CMD.zip     .\CompiledBIN_CMD\*.exe   .\CompiledBIN_CMD\*.dll
+"%PROGRAMFILES%\7-Zip\7z.exe"    a   GDocBackup_$$$VerMajor$$$.$$$VerMinor$$$.$$$VerBuild$$$.$$$VerRevision$$$_BIN.zip     .\CompiledBIN\*.exe   .\CompiledBIN\*.dll
+"%PROGRAMFILES%\7-Zip\7z.exe"    a   GDocBackup_$$$VerMajor$$$.$$$VerMinor$$$.$$$VerBuild$$$.$$$VerRevision$$$_CMD.zip     .\CompiledBIN_CMD\*.exe   .\CompiledBIN_CMD\*.dll
 
