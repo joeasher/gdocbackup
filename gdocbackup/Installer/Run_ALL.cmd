add9f57e009b0dabc367d75a770b5f2f1574c583
fhtino <fhtino@gmail.com>
2011-08-28 19:29:11 +0000

    [issue 45] compiler and deploy script update

--- gdocbackup/Installer/Run_ALL.cmd
+++ gdocbackup/Installer/Run_ALL.cmd
@@ -19,6 +19,7 @@ xcopy ..\GDocBackup\bin\Release\Google.GData.Client.dll         .\CompiledBIN\  
 xcopy ..\GDocBackup\bin\Release\Google.GData.Documents.dll      .\CompiledBIN\  /Y
 xcopy ..\GDocBackup\bin\Release\Google.GData.Extensions.dll     .\CompiledBIN\  /Y
 xcopy ..\GDocBackup\bin\Release\Google.GData.AccessControl.dll  .\CompiledBIN\  /Y
+xcopy ..\GDocBackup\bin\Release\Google.GData.Apps.dll           .\CompiledBIN\  /Y
 
 @echo ----------------------
 
@@ -28,6 +29,7 @@ xcopy ..\GDocBackupCMD\bin\Release\Google.GData.Client.dll         .\CompiledBIN
 xcopy ..\GDocBackupCMD\bin\Release\Google.GData.Documents.dll      .\CompiledBIN_CMD\  /Y
 xcopy ..\GDocBackupCMD\bin\Release\Google.GData.Extensions.dll     .\CompiledBIN_CMD\  /Y
 xcopy ..\GDocBackupCMD\bin\Release\Google.GData.AccessControl.dll  .\CompiledBIN_CMD\  /Y
+xcopy ..\GDocBackupCMD\bin\Release\Google.GData.Apps.dll           .\CompiledBIN_CMD\  /Y
 
 @echo ----------------------
 
